4021517a56bb4f8c062ca3b34e5b94fc9f0dd709
crusiris <cruzsirys@gmail.com>
2019-04-20 08:01:01 +0200

    Obteniendo informacion del formulario con ngModel

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,16 +1,28 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
+interface Usuario {
+  nombre: string;
+  apellido: string;
+  correo: string;
+}
+
 @Component({
   selector: "app-template",
   templateUrl: "./template.component.html",
   styles: []
 })
 export class TemplateComponent {
+  usuario: Usuario = {
+    nombre: "Crusiris",
+    apellido: "Patriz",
+    correo: ""
+  };
   constructor() {}
 
   guardar(forma: NgForm) {
-    console.log("Formulario Posteado");
-    console.log(forma);
+    console.log("NgForm", forma);
+    console.log("Valor Forma", forma.value);
+    console.log("usuario", this.usuario);
   }
 }
