466166e0c39121dd99bf9cb5f422c567d39498ac
crusiris <cruzsirys@gmail.com>
2019-04-21 07:08:30 +0200

    Formulario en data

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-data',
-  templateUrl: './data.component.html',
+  selector: "app-data",
+  templateUrl: "./data.component.html",
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
+  forma: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.forma = new FormGroup({
+      nombre: new FormControl("Crusiris"),
+      apellido: new FormControl(),
+      correo: new FormControl()
+    });
   }
 
+  guardarCambios() {
+    console.log(this.forma.value);
+    console.log(this.forma);
+  }
 }
