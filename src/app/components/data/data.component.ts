b1301fe95ac9d6a78abc6b6b5f66ffee7040d45d
crusiris <cruzsirys@gmail.com>
2019-04-21 07:30:46 +0200

    Enlazando Inputs a propiedades de formGroup y validaciones

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -11,9 +11,12 @@ export class DataComponent {
 
   constructor() {
     this.forma = new FormGroup({
-      nombre: new FormControl("Crusiris"),
-      apellido: new FormControl(),
-      correo: new FormControl()
+      nombre: new FormControl("", Validators.required),
+      apellido: new FormControl("", Validators.required),
+      correo: new FormControl("", [
+        Validators.required,
+        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$")
+      ])
     });
   }
 
